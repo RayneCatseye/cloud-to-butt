b5dddeef56ae199ffbb43859f46dc7ac13f8d2e0
Emily Saunders Walmsley <EmSaundWalm@gmail.com>
2015-03-18 23:00:48 +1100

    Recompiled for addition to Chrome

--- Source/content_script.js
+++ Source/content_script.js
@@ -33,16 +33,18 @@ function handleText(textNode)
 
 	v = v.replace(/\bFarmer John\b/g, "John Peters, you know, the farmer");
 	v = v.replace(/\bFarmer john\b/g, "John Peters, you know, the farmer");
-	v = v.replace(/\bframer John\b/g, "John Peters, you know, the farmer");
+	v = v.replace(/\bfarmer John\b/g, "John Peters, you know, the farmer");
 	v = v.replace(/\bfarmer john\b/g, "John Peters, you know, the farmer");
 	v = v.replace(/\bFJ\b/g, "John Peters, you know, the farmer");
+
+
 
 	v = v.replace(/\bBessie\b/g, "his invisible corn");
 	v = v.replace(/\bbessie\b/g, "his invisible corn");
 	v = v.replace(/\bBessy\b/g, "his invisible corn");
 	v = v.replace(/\beessy\b/g, "his invisible corn");
-	v = v.replace(/\bBetsie\b/g, "his invisible corn");
-	v = v.replace(/\bbetsie\b/g, "his invisible corn");
+	v = v.replace(/\bBetsy\b/g, "his invisible corn");
+	v = v.replace(/\bbetsy\b/g, "his invisible corn");
 	
 	textNode.nodeValue = v;
 }
