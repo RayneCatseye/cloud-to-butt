00ad2b6551739733d384b62216c3bdfbaff83fbf
Emily Saunders Walmsley <RayneCatseye@users.noreply.github.com>
2015-03-18 15:43:15 +1100

    Removed cows, added invisible corn

--- Source/content_script.js
+++ Source/content_script.js
@@ -36,6 +36,13 @@ function handleText(textNode)
 	v = v.replace(/\bframer John\b/g, "John Peters, you know, the farmer");
 	v = v.replace(/\bfarmer john\b/g, "John Peters, you know, the farmer");
 	
+	v = v.replace(/\bBessie\b/g, "his invisible corn");
+	v = v.replace(/\bbessie\b/g, "his invisible corn");
+	v = v.replace(/\bBessy\b/g, "his invisible corn");
+	v = v.replace(/\beessy\b/g, "his invisible corn");
+	v = v.replace(/\bBetsie\b/g, "his invisible corn");
+	v = v.replace(/\bbetsie\b/g, "his invisible corn");
+	
 	textNode.nodeValue = v;
 }
 
